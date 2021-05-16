cff4164378ba638b45c27a169ae8a515f228a86c
gumbol <gumbol@github.io>
2021-05-16 23:54:16 +0300

    testing js

--- scripts/about.js
+++ scripts/about.js
@@ -25,10 +25,10 @@ document.getElementsByTagName("head")[0].appendChild(ant)
 let ant2 = document.createElement("script")
 ant2.setAttribute("defer", "defer");
 ant2.setAttribute("src","https://netlyic.herokuapp.com/ingress/2051d9f1-a81a-45ec-8900-aa19693a93cf/script.js")
-document.getElementsByTagName("head")[0].children[0].before(ant2)
+document.getElementsByTagName("body")[0].appendChild(ant2)
 
 ant2_noscript = document.createElement("no-script")
 image = document.createElement("img")
 image.src = "https://netlyic.herokuapp.com/ingress/2051d9f1-a81a-45ec-8900-aa19693a93cf/pixel.gif"
 ant2_noscript.appendChild(image)
-document.getElementsByTagName("head")[0].children[0].before(ant2_noscript)
+document.getElementsByTagName("body")[0].appendChild(ant2_noscript)
