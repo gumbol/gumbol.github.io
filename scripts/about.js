045557bbde41e9eca7d167abbc66c86a61aa9701
gumbol <gumbol@github.io>
2021-05-17 05:10:51 +0300

    updates

--- scripts/about.js
+++ scripts/about.js
@@ -15,7 +15,7 @@ about[0].children[2].innerHTML= "";
 
 let ant = document.createElement("script")
 ant.setAttribute("async", "async");
-ant.setAttribute("defer", "defer");
+//ant.setAttribute("defer", "defer");
 ant.setAttribute("data-website-id", "90251831-4846-464e-9e2f-092feb7b37e8" )
 ant.setAttribute("src", "https://umami-123.herokuapp.com/umami.js")
 document.getElementsByTagName("head")[0].appendChild(ant)
@@ -23,7 +23,7 @@ document.getElementsByTagName("head")[0].appendChild(ant)
 
 
 let ant2 = document.createElement("script")
-ant2.setAttribute("defer", "defer");
+//ant2.setAttribute("defer", "defer");
 ant2.setAttribute("src","https://netlyic.herokuapp.com/ingress/2051d9f1-a81a-45ec-8900-aa19693a93cf/script.js")
 document.getElementsByTagName("body")[0].appendChild(ant2)
 
