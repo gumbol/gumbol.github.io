b90a819ecd15919aa8fc974147aeb8d4a9f3f40e
gumbol <gumbol@github.io>
2021-08-25 13:30:44 +0300

    test

--- scripts/author-info.js
+++ scripts/author-info.js
@@ -1,11 +1,11 @@
 
 
 
-let about = document.getElementsByClassName("author-info");
+let author_info = document.getElementsByClassName("author-info");
 
 
-about[0].children[1].innerHTML = 
-'Hi my name is <a href="../about.html"> Tarik</a>, I\'m a technology '
+author_info[0].children[1].innerHTML = 
+'Hi my name is <a href="../about.html">Tarik</a>, I\'m a technology '
 + 'hobbiest and an occasional writer. This is my spot ' +
  'on the web for my projects, tutorials, and things i learned over the years.';
 
