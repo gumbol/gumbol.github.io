dae29bb6b45124963ac70da9d019f3fa83baa716
gumbol <gumbol@github.io>
2021-09-21 08:18:39 +0300

    post improvements

--- scripts/author-info.js
+++ scripts/author-info.js
@@ -5,14 +5,6 @@ let author_info = document.getElementsByClassName("author-info");
 
 
 author_info[0].children[1].innerHTML = 
-'Hi my name is <a href="../about.html">Tarik</a>, I\'m a technology '
+'<p>Hi my name is <a href="../about.html">Tarik</a>, I\'m a technology '
 + 'hobbiest and an occasional writer. This is my spot ' +
- 'on the web for my projects, tutorials, and things i learned over the years.';
-
-
-
-//if(about[0].children[i] == "a")
-//about[0].children[i].innerHTML= "";
-/*about[0].children[2].innerHTML= ' <a class="coffee-btn"' +
-'href="https://ko-fi.com/tyah2" target="_blank"' + 
-'rel="noreferrer">Buy me a coffee </a>'*/
+ 'on the web for my projects, tutorials, and things i learned over the years.</p>';
