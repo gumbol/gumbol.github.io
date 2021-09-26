a8f18fe1f525501446e5b97c09ffbf99be385e1a
gumbol <gumbol@github.io>
2021-09-26 16:24:21 +0300

    style and content improvments

--- scripts/author-info.js
+++ scripts/author-info.js
@@ -3,9 +3,6 @@
 
 let author_info = document.getElementsByClassName("author-info");
 
-author_info[0].children[3].innerHTML = 
-'Hi my name is <a href="../about.html">Tarik</a>, I\'m a technology '
-+ 'hobbiest since the late 90\'s and an occasional writer. This is my spot ' +
- 'on the web for my projects, tutorials, and things i learned over the years.';
+author_info[0].children[3].innerHTML = "Hi my name is <a href='../about.html'>Tarik</a> I am a technology hobbiest, open-source creator and an occasional writer. This is my spot on the web for my projects and things i learned over the years";
 
 
